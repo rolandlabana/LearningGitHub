62a44f5fd4ea3ea8186090e7b5de85aaa6320eed
Roland Labana <rolandlabana@gmail.com>
2023-01-02 11:58:53 -0800

    Create MainProgram.py
    created this file as the basis for learning GitHub

---
+++ MainProgram.py
@@ -0,0 +1,8 @@
+#This is our main program to learn GitHub basics
+#all it does it print a welcome message and everyone's name who has added a commit to this file
+
+print ("Welcome to learning GitHub with Mr. Labana")
+print ("Here is everyone who has added to this file....")
+print ()
+print ("Roland Labana-RSL on Jan 2, 2023")
+print ("That's everyone who has committed a change to this file!")
