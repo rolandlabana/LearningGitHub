56d0a009f36ac625e0eaf75509d8871bdfd13236
Ali-Panagia <167352099+Ali-Panagia@users.noreply.github.com>
2024-04-18 11:10:14 -0700

    Update MainProgram.py (#72)
    Co-authored-by: Roland Labana <rolandlabana@gmail.com>

--- MainProgram.py
+++ MainProgram.py
@@ -34,6 +34,7 @@ print ("2024 Semesters:")
 print("Dominic Saienni Spring term 2024")
 print ("Judah Johnson")
 print ("Basheer Maalouf, Spring Term/2024")
+print ("Ali Panagia, Spring Term/2024")
 print("Cole Sordal, Spring Term/2024")
 
 print ("That's everyone who has committed a change to this file!")
